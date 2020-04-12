85b6bdd4798af261634e3ad021015d07e432ad49
anoncrotz <55352652+anoncrotz@users.noreply.github.com>
2020-04-12 15:26:24 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,13 @@
+<?php
+
+#MASUKAN COOKIE CFDUID MILIK KALIAN!!!
+$cfduid = "xxxxxxxxx";
+
+#MASUKAN COOKIE PHPSESSID MILIK KALIAN!!!
+$PHPSESSID = "xxxxxxxx";
+
+#MASUKAN COOKIE GA MILIK KALIAN!!!
+$ga = "xxxxxxxxx";
+
+#MASUKAN COOKIE GID MILIK KALIAN!!!
+$gid = "xxxxxxxxx";
